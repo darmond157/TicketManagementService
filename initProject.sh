b2ecea5869c1c89d750d6a4329953511bb1fd9ee
Bardia <bardiarezaeikalantari@gmail.com>
2024-02-07 18:20:53 +0330

    build: change folder name

--- initProject.sh
+++ initProject.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd backend
+cd Nodejs
 docker build -t backend-image .
 docker run -d --name backend-container backend-image
 
-cd ../frontend
+cd ../Vue
 docker build -t frontend-image .
 docker run -d --name frontend-container frontend-image
