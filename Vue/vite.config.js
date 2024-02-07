370852fad8c454c4c9a2844326297fad770ee790
Bardia <bardiarezaeikalantari@gmail.com>
2024-02-08 01:41:51 +0330

    chore: remove host env

--- Vue/vite.config.js
+++ Vue/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	server: {
-		host: process.env.VITE_HOST,
+		host: "0.0.0.0",
 	},
 	plugins: [vue()],
 	resolve: {
